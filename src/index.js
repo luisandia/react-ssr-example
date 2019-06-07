1aa7f55510bdb0cf35fecefc539efbebd4431582
luisandia <luisandiamamani@gmail.com>
2019-06-07 01:48:19 +0000

    init proxy to security

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
+import proxy from 'express-http-proxy';
+
 const app = express();
+
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+        opts.header['x-forwarded-host'] = 'localhost:3000';
+        return opts;
+    }
+}));
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
